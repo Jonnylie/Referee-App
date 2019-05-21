232bfdf90626f9af3b183b2ecab78ab1c0d6f913
Danny Jac <42330274+DannyJac@users.noreply.github.com>
2019-05-21 13:32:04 +1000

    Comparify Xcode

---
+++ Comparify/Comparify/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Comparify
+//
+//  Created by WOLF on 21/5/19.
+//  Copyright © 2019 Group-127. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

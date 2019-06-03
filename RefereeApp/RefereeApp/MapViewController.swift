1623b91baaf5a4bce960ba8e31be284a3b8e8039
Jonny <jonny.lie@student.uts.edu.au>
2019-06-03 19:11:09 +1000

    Map Kit Implementation

---
+++ RefereeApp/RefereeApp/MapViewController.swift
@@ -0,0 +1,98 @@
+//
+//  MapViewController.swift
+//  Referee
+//
+//  Created by Erawati Tjahjadi on 02/06/19.
+//  Copyright © 2019 Franco Buena. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class customPin: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(pinTitle:String, pinSubTitle:String, location:CLLocationCoordinate2D) {
+        self.title = pinTitle
+        self.subtitle = pinSubTitle
+        self.coordinate = location
+    }
+}
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    @IBOutlet weak var label: UILabel!
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegion(center: myLocation, span: span)
+        map.setRegion(region, animated: true)
+        self.map.showsUserLocation = true
+        
+        let pin = customPin(pinTitle: "Match is happening", pinSubTitle: "Team 1 vs Team 2", location: myLocation)
+        self.map.addAnnotation(pin)
+        self.map.delegate = self
+    
+        
+        
+        CLGeocoder().reverseGeocodeLocation(location) { (placemark, error) in
+            if error != nil
+            {
+                print ("There was an error")
+            }
+            else
+            {
+                if let place = placemark?[0]
+                {
+                    if place.locality != nil
+                    {
+                    self.label.text = "\(place.locality!) \n \(place.subLocality!) \n \(place.country!)"
+                    }
+                }
+            }
+        }
+        }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+       
+       
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "customanotation")
+        annotationView.image = UIImage(named:"pin")
+        
+        annotationView.canShowCallout = true
+        return annotationView
+    }
+    
+   
+}
+
+
+    
+
+    
+

bc1abffc15e09e78da421b9a0eb4dac24b591b6e
Danny Jac <42330274+DannyJac@users.noreply.github.com>
2019-06-04 15:03:29 +1000

    confused commit

--- RefereeApp/RefereeApp/TeamOneTableViewController.swift
+++ RefereeApp/RefereeApp/TeamOneTableViewController.swift
@@ -11,49 +11,49 @@ import UIKit
 
 class TeamOneTableViewController: UITableViewController {
     
-    var teamOne: [Player] = []
-
+    var homeTeam: [Player] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        teamOne = createTeam()
+        homeTeam = createTeam()
     }
     
     func createTeam() -> [Player] {
-        var teamOnePlayers: [Player] = []
+        var teamHomePlayers: [Player] = []
         
-        teamOnePlayers.append(Player(number: "13", name: "Alisson", position: "GK"))
-        teamOnePlayers.append(Player(number: "66", name: "Alexander-Arnold", position: "RB"))
-        teamOnePlayers.append(Player(number: "32", name: "Matip", position: "CB"))
-        teamOnePlayers.append(Player(number: "4", name: "Van Dyke", position: "CB"))
-        teamOnePlayers.append(Player(number: "26", name: "Robertson", position: "LB"))
-        teamOnePlayers.append(Player(number: "14", name: "Henderson", position: "MF"))
-        teamOnePlayers.append(Player(number: "3", name: "Fabinho", position: "CM"))
-        teamOnePlayers.append(Player(number: "7", name: "Milner", position: "MF"))
-        teamOnePlayers.append(Player(number: "23", name: "Shaqiri", position: "RW"))
-        teamOnePlayers.append(Player(number: "27", name: "Origi", position: "ST"))
-        teamOnePlayers.append(Player(number: "10", name: "Mane", position: "LW"))
+        teamHomePlayers.append(Player(number: 13))
+        teamHomePlayers.append(Player(number: 66))
+        teamHomePlayers.append(Player(number: 32))
+        teamHomePlayers.append(Player(number: 4))
+        teamHomePlayers.append(Player(number: 26))
+        teamHomePlayers.append(Player(number: 14))
+        teamHomePlayers.append(Player(number: 3))
+        teamHomePlayers.append(Player(number: 7))
+        teamHomePlayers.append(Player(number: 23))
+        teamHomePlayers.append(Player(number: 27))
+        teamHomePlayers.append(Player(number: 10))
         
-        return teamOnePlayers
+        return teamHomePlayers
     }
-
-
+    
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return teamOne.count
+        return homeTeam.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let team = teamOne[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "teamOne", for: indexPath) as! PlayerCell
-        cell.setTeamOnePlayer(player: team)
+        let team = homeTeam[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: home, for: indexPath) as! PlayerCell
+        cell.setHomePlayer(player: team)
         return cell
     }
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?{
-        return "Home"
+        return home
     }
 }
